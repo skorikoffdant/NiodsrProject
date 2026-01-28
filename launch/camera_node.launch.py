fa5ef2a8ff9fa846133930a18fc4f31e2b22b524
Maryna Iovitsa <iovitsa.marina@gmail.com>
2026-01-28 13:25:59 +0100

    RViz and URSim commit to update launch.py

--- launch/camera_node.launch.py
+++ launch/camera_node.launch.py
@@ -1,23 +1,47 @@
-#Updated by Maryna Iovitsa
+# Updated by Maryna Iovitsa
 
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-import os
+
 
 def generate_launch_description():
 
     return LaunchDescription([
 
+        # USB camera
         Node(
             package='usb_cam',
             executable='usb_cam_node_exe',
             output='screen'
         ),
+
+        # Camera subscriber
         Node(
             package='camera_subscriber',
             executable='camera_node',
             output='screen'
+        ),
+
+        # Start URSim (UR5)
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'run', 'ur_client_library',
+                'start_ursim.sh',
+                '-m', 'ur5'
+            ],
+            output='screen'
+        ),
+
+        # Start UR driver with RViz
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'launch', 'ur_robot_driver',
+                'ur_control.launch.py',
+                'ur_type:=ur5',
+                'robot_ip:=192.168.56.101',
+                'launch_rviz:=true'
+            ],
+            output='screen'
         )
     ])
-
